23ae3c89e706d82519d891deb1884a451df0d5fd
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2025-04-29 14:35:47 +0200

    Add app arguments for URL reading feature

--- tileScraper/src/main/java/com/example/tileScraper/tileScraper/scraperService/ScraperService.java
+++ tileScraper/src/main/java/com/example/tileScraper/tileScraper/scraperService/ScraperService.java
@@ -6,26 +6,55 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
-public class ScraperService {
-    private static final String URL = "https://relatedwords.org/relatedto/";
-    private final ChromeDriver driver = new ChromeDriver();
+public class ScraperService implements CommandLineRunner {
+    private final ApplicationArguments arguments;
+    private final ChromeDriver driver =  new ChromeDriver();
 
-    @PostConstruct
-    void postConstruct() {
-        scrape("fishsticks");
+    @Value("${scraper.baseUrl:https://www.google.com/}")
+    private String URL = null;
+
+
+    public ScraperService(ApplicationArguments arguments) {
+        this.arguments = arguments;
     }
 
-    public  void scrape(final String value){
-        driver.get(URL + value);
-        final WebElement words = driver.findElement(By.className("words"));
-        final List<WebElement> wordList = words.findElements((By.tagName("a")));
-        wordList.forEach((word -> System.out.println(word.getText())));
-        driver.quit();
+    @Override
+    public void run(String... args) throws Exception {
+        if (arguments.containsOption("arg1")) {
+            System.out.println("arg1 value" + arguments.getOptionValues("arg1").get(0));
+            URL = arguments.getOptionValues("arg1").get(0);
+        }
+//     ChromeDriver driver = new ChromeDriver();
+        try{
+            String searchTerm = "fishsticks";
+            if(arguments.containsOption("search")) {
+                URL = arguments.getOptionValues("search").get(0);
+            }
+            scrape(searchTerm);
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+
+    public void scrape(final String value) {
+        try {
+            System.out.println("Scraping with URL: " + URL + " and search term: " + value);
+            driver.get(URL + value);
+            final WebElement words = driver.findElement(By.className("words"));
+            final List<WebElement> wordList = words.findElements((By.tagName("a")));
+            wordList.forEach((word -> System.out.println(word.getText())));
+        } catch (Exception e) {
+            System.err.println("Error during scraping: " + e.getMessage());
+        }
     }
 }
--- tileScraper/src/main/java/com/example/tileScraper/tileScraper/seleniumConfiguration/SeleniumConfiguration.java
+++ tileScraper/src/main/java/com/example/tileScraper/tileScraper/seleniumConfiguration/SeleniumConfiguration.java
@@ -2,6 +2,7 @@ package com.example.tileScraper.tileScraper.seleniumConfiguration;
 
 import jakarta.annotation.PostConstruct;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,9 @@ public class SeleniumConfiguration {
 
     @Bean
     public ChromeDriver ChromeDriver() {
-        return new ChromeDriver();
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+
+        return new ChromeDriver(chromeOptions);
     }
 }
